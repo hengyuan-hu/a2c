5b32df7c5c2c173cb8af7801a0a23df293ab216d
hengyuan-hu <kevin.huhy@gmail.com>
2018-01-18 00:35:53 +0800

    rename.

--- network.py
+++ network.py
@@ -4,12 +4,12 @@ import utils
 
 
 class ActorCriticNetwork(nn.Module):
-    def __init__(self, conv, fc, value, policy):
+    def __init__(self, conv, fc, value, pi_logit):
         super(ActorCriticNetwork, self).__init__()
         self.conv = conv
         self.fc = fc
         self.value = value
-        self.policy = policy
+        self.pi_logit = pi_logit
 
     def forward(self, x):
         assert x.data.max() <= 1.0
@@ -19,8 +19,8 @@ class ActorCriticNetwork(nn.Module):
         feat = feat.view(batch, -1)
         feat = self.fc(feat)
         val = self.value(feat)
-        pi = self.policy(feat)
-        return val, pi
+        pi_logit = self.pi_logit(feat)
+        return val, pi_logit
 
 
 # ---------------------------------------
@@ -37,7 +37,7 @@ def _build_default_conv(in_channels):
 
 
 def _build_fc(dims, num_actions, noise_std):
-    if noise_std > 0:
+    if noise_std is not None and noise_std > 0:
         layer_func = lambda indim, outdim: NoisyLinear(indim, outdim, noise_std)
     else:
         layer_func = nn.Linear
@@ -49,9 +49,8 @@ def _build_fc(dims, num_actions, noise_std):
 
     fc = nn.Sequential(*layers)
     value = layer_func(dims[-1], 1)
-    policy = nn.Sequential(
-        layer_func(dims[-1], num_actions), nn.Softmax(dim=1))
-    return fc, value, policy
+    pi_logit = layer_func(dims[-1], num_actions)
+    return fc, value, pi_logit
 
 
 def build_default_network(in_channels, in_size, num_actions, noise_std, net_file):
@@ -60,9 +59,9 @@ def build_default_network(in_channels, in_size, num_actions, noise_std, net_file
     in_shape = (1, in_channels, in_size, in_size)
     fc_in = utils.count_output_size(in_shape, conv)
     fc_hid = 512
-    fc, value, policy = _build_fc([fc_in, fc_hid], num_actions, noise_std)
+    fc, value, pi_logit = _build_fc([fc_in, fc_hid], num_actions, noise_std)
 
-    net = ActorCriticNetwork(conv, fc, value, policy)
+    net = ActorCriticNetwork(conv, fc, value, pi_logit)
     utils.init_net(net, net_file)
     return net
 
